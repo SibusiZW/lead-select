cb02ef4796bd9bcb29c301778105aa31393b37ab
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-18 16:46:31 +0200

    feat: imported models into views.py continuing later

--- lead_select/core/views.py
+++ lead_select/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from .models import Election, Candidate, Vote
 from .forms import LoginForm
 
 def login_view(request):
