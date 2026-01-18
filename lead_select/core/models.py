e570469ff07bcdafe169c8920e9218e7d1fe38cb
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-18 16:20:16 +0200

    feat: added Election, Candidate, and Vote models

--- lead_select/core/models.py
+++ lead_select/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Election(models.Model):
+    title = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Candidate(models.Model):
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='votes')
+    election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='votes')
+
+    class Meta:
+        unique_together = ('user', 'election')
+
+    def __str__(self):
+        return f"{self.candidate} - {self.user}"
